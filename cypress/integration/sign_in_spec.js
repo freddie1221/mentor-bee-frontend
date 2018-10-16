586c53641944c980a62c09ef2c0991a51addc687
leekarho <k-h_lee@hotmail.co.uk>
2018-10-16 17:55:15 +0100

    test: trying to pass sign in test

--- cypress/integration/sign_in_spec.js
+++ cypress/integration/sign_in_spec.js
@@ -1,6 +1,6 @@
 describe('visit homepage', function() {
   it('visits homepage', function() {
     cy.visit('localhost:3000/#')
-    cy.contains('Email')
-    cy.contains('Password')
+    const form = cy.get(form)
+    form.find('input').should('contain', 'placeholder', 'Email');
   })
