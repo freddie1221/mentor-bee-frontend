2ea229dd12364afc89f0f5cc8c8f6c0a2321539b
Freddie <fredjlawson@gmail.com>
2018-10-16 11:37:53 +0100

    feat: add registration and sign in basic views

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,30 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+
+
+describe('visit homepage', function() {
+  it('visits homepage', function() {
+    cy.visit('localhost:3000/#')
+    cy.contains('Sign in')
+    cy.contains('Register')
   })
+  it('loads the registration page when register is clicked', function() {
+    cy.visit('localhost:3000/#')
+    cy.contains('Register').click()
+    cy.contains('Please register here')
+  })
+})
+
+describe('sign in component', function(){
+  it('has the expected fields', function() {
+    cy.visit('localhost:3000/#')
+    cy.contains('Register').click()
+    cy.contains('Name')
+    cy.contains('Email')
+    cy.contains('Password')
+    cy.contains('Password confirmation')
+  })
+
+})
+
+describe('sign in form', function() {
+
 })
---
+++ src/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Please register here</p>
+        {/* Need to make a react form */}
+      </div>
+    );
+  }
+}
+
+export default SignIn;
