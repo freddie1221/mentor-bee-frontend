dd3d15a8282516813be08f3e5bde4b90640171b7
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-18 12:08:02 +0100

    test: setup all_mentors link-to-profile test

--- cypress/integration/all_mentors_spec.js
+++ cypress/integration/all_mentors_spec.js
@@ -4,4 +4,11 @@ describe("/mentors", function() {
     cy.get(".card-wrapper")
     cy.should("contain", "Newt Scamander")
   });
+
+  it("mentor card contains link to mentor profile", function() {
+    cy.visit("localhost:3000/#/mentors")
+    cy.get(".profile-link").click()
+    cy.should("contain", "Profile")
+
+  })
 });
