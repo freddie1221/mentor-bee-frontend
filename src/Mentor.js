d3224897d31e99df7f821fdfd46d8a23c1239d4d
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-18 15:51:31 +0100

    feat: click on mentor on /mentors page to go to their profile

--- src/Mentor.js
+++ src/Mentor.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ProfilePage from "./ProfilePage";
 
 class Mentor extends React.Component {
   render() {
--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,10 +1,43 @@
 import React from "react";
 
 class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mentor: []
+    }
+  }
+
+  componentDidMount() {
+    const address = this.props.location.pathname
+    const idString = address.split("mentors/")[1]
+
+    const url = `https://mentor-bee.herokuapp.com/users/${idString}`
+
+    console.log(url)
+
+    fetch(url)
+    .then(response => {
+      return response.json()
+    }).then(data => {
+      this.setState({
+        mentor: data
+      })
+    })
+  }
+
+
   render() {
 
+    const name = this.state.mentor.name
+    const pic = this.state.mentor.pic
+
     return (
-      <h1>Profile Page</h1>
+      <div>
+        <h2>{name}'s Profile</h2>
+        <img alt="profile-pic" src={pic}></img>
+      </div>
     )
   }
 }
