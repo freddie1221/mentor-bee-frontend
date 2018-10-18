0bc00b058d0d33ab43e771cce14e9fdb39f4ad6a
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-18 16:51:31 +0100

    refactor: update specs, delete un-needed specs

--- cypress/integration/all_mentors_spec.js
+++ cypress/integration/all_mentors_spec.js
@@ -2,6 +2,6 @@ describe("/mentors", function() {
   it("displays all mentors", function() {
     cy.visit("localhost:3000/#/mentors")
     cy.get(".card-wrapper")
-    cy.should("contain", "Newt Scamander")
+    cy.should("contain", "Peeves")
   });
 });
