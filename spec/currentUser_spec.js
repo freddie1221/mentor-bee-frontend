45d447720dec6c3eaf86cfd0a7e7be7c7dbb9140
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-21 16:20:05 +0100

    feat: can getEmail and setEmail

--- spec/currentUser_spec.js
+++ spec/currentUser_spec.js
@@ -11,4 +11,18 @@ describe(CurrentUser, function() {
   it("has a name", function() {
     expect(currentUser.getName()).toEqual("Alice");
   });
+  
+  it("can change a name", function() {
+    currentUser.setName("Bob")
+    expect(currentUser.getName()).toEqual("Bob");
+  });
+
+  it("has an email", function() {
+    expect(currentUser.getEmail()).toEqual("ab@cd.com");
+  });
+
+  it("can change an email", function() {
+    currentUser.setEmail("123@abc.com")
+    expect(currentUser.getEmail()).toEqual("123@abc.com");
+  });
 });
--- src/CurrentUser.js
+++ src/CurrentUser.js
@@ -1,9 +1,22 @@
-function CurrentUser(name) {
+function CurrentUser(name, email) {
   this._name = name;
+  this._email = email;
 }
   
 CurrentUser.prototype.getName = function() {
   return this._name;
+}
+
+CurrentUser.prototype.setName = function(updatedName) {
+  this._name = updatedName
+}
+
+CurrentUser.prototype.getEmail = function() {
+  return this._email;
+}
+
+CurrentUser.prototype.setEmail = function(updatedEmail) {
+  this._email = updatedEmail
 }
 
 module.exports = CurrentUser;
