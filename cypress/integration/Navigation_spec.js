282ed44a111019557d3e75007101d9eddee78af0
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-22 16:26:41 +0100

    test: refactor spec in light of conditional NavBar

--- cypress/integration/Navigation_spec.js
+++ cypress/integration/Navigation_spec.js
@@ -2,12 +2,7 @@ describe('navigation', function() {
   beforeEach(function(){
     cy.visit('/')
   })
-  describe('profile button', function() {
-    it('can take you to profile page', function() {
-      cy.contains('Profile').click()
-      cy.url().should('include', '/profile')
-    })
-  })
+  
   describe('sign in button', function() {
     it('can take you to the sign in page', function() {
       cy.contains('Sign in').click()
