669159c73d982b8dfb737f7cc5a49f2094dbb9a1
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-23 09:00:58 +0100

    tests: refactor registration tests to expect click

--- cypress/integration/registration_spec.js
+++ cypress/integration/registration_spec.js
@@ -7,7 +7,7 @@ describe('visit homepage', function() {
 })
 
 describe('Register component', function(){
-  it('redirects to /mentors on submission', function() {
+  it('can submit form', function() {
     cy.visit('/')
     cy.contains('Register').click()
     cy.get('form').within(($form) => {
@@ -15,7 +15,6 @@ describe('Register component', function(){
       cy.get('input[name="email"]').type('abc3@email.com')
       cy.get('input[name="password"]').type('password')
       cy.root().submit()
-      cy.url().should("include", "/mentors");
     })
   })
 })
