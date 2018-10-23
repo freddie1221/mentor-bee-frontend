e30e77e9624e6e03328b68faa0cd23827de12d63
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-23 15:27:05 +0100

    fix: add name into Mentor

--- src/components/Mentor.js
+++ src/components/Mentor.js
@@ -5,6 +5,7 @@ class Mentor extends React.Component {
     const userId = this.props.mentor.user_id
     const pic = this.props.mentor.pic
     const skill = this.props.mentor.skill
+    const name = this.props.mentor.names
 
     return (
       <div>
@@ -12,7 +13,7 @@ class Mentor extends React.Component {
           <div className="card">
             <img alt="profile-pic" className="profile-pic" src={pic}></img>
             <div className="card-desc">
-              <p className="card-name">{userId}</p>
+              <p className="card-name">{name}</p>
               <p className="card-skills"><span className="skills-title">Available to mentor in:</span> {skill}</p>
             </div>
           </div>
