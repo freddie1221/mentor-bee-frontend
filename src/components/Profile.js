2ce2858e6a34ecc6bd7cf006a58bdd49fbddcc7d
Freddie <fredjlawson@gmail.com>
2018-10-23 15:44:33 +0100

    feat: adds conditional logic to profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,49 +1,31 @@
-import React, { Component } from "react";
-import MentorRegistration from './MentorRegistration'
-import MenteeRegistration from './MenteeRegistration'
+import React, { Component } from "react"
+import CreateProfile from "./CreateProfile"
+import PersonalProfile from "./PersonalProfile"
 
 class Profile extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      mentorCreateUrl: 'https://mentor-bee-api.herokuapp.com/mentors',
-      menteeCreateUrl: 'https://mentor-bee-api.herokuapp.com/mentees',
-      mentorForm: '',
-      menteeForm: ''
+      isMentor: true,
+      isMentee: false
     }
-    this.mentor = this.mentor.bind(this)
-    this.mentee = this.mentee.bind(this)
-  }
-  
-  mentor() {
-    this.setState({
-      mentorForm: <MentorRegistration />,
-      menteeForm: ''
-    })
+    this.isNotMenteeOrMentor = this.isNotMenteeOrMentor.bind(this)
   }
 
-  mentee() {
-    this.setState({
-      menteeForm: <MenteeRegistration />,
-      mentorForm: ''
-    })
+  isNotMenteeOrMentor() {
+    if (this.state.isMentor === false && this.state.isMentor === false) {
+      return <CreateProfile />
+    }
+    return <PersonalProfile />
   }
-
-  render () {
+        
+  render() {
     return (
-      <div className="profile-wrapper">
-        <form>
-          <button onClick={this.mentor}>Become a mentor</button>
-          {this.state.mentorForm}
-        </form>
-        <form>
-        <button onClick={this.mentee}>Become a mentee</button>
-          {this.state.menteeForm}
-        </form>
+      <div>
+        {this.isNotMenteeOrMentor()}
       </div>
     )
   }
 }
 
-export default Profile;
+export default Profile
