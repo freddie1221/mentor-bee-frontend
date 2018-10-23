180dba5f622c1d7b44558f822071213f462066a9
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-23 13:56:34 +0100

    fix: update Mentor with mentor data, update href link to use correct url, add auth token to fetch on ProfilePage

--- src/components/Mentor.js
+++ src/components/Mentor.js
@@ -2,18 +2,18 @@ import React from "react";
 
 class Mentor extends React.Component {
   render() {
-    const name = this.props.mentor.user_id
+    const userId = this.props.mentor.user_id
     const pic = this.props.mentor.pic
     const skill = this.props.mentor.skill
     const id = this.props.mentor.id
 
     return (
       <div>
-        <a href={`/#mentors/${id}`}>
+        <a href={`/#mentors/${userId}`}>
           <div className="card">
             <img alt="profile-pic" className="profile-pic" src={pic}></img>
             <div className="card-desc">
-              <p className="card-name">{name}</p>
+              <p className="card-name">{userId}</p>
               <p className="card-skills"><span className="skills-title">Available to mentor in:</span> {skill}</p>
             </div>
           </div>
