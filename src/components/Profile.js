0871051101f7485e53e9813ff16daaf97db78785
Freddie <fredjlawson@gmail.com>
2018-10-24 17:33:22 +0100

    feat: profile now shows if it exists

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,21 +3,33 @@ import CreateProfile from "./CreateProfile"
 import PersonalProfile from "./PersonalProfile"
 
 class Profile extends Component {
-
-  isNotMenteeOrMentor() {
-    const user = JSON.parse(window.localStorage.getItem("currentUser"))
-    if (user._menteeBio === "" && user._mentorBio === "") {
-      return <CreateProfile />
+  constructor(props) {
+    super(props)
+    this.state = {
+      profileExists: false
     }
-    return <PersonalProfile />
+    this.profileCreated = this.profileCreated.bind(this)
   }
+
+  profileCreated(){
+    this.setState({ profileExists: true })
+  }
+
+  componentDidMount() {
+    if (window.localStorage.getItem("currentUser")) {
+      this.setState({ profileExists: true })
+    }
+  }
+
         
   render() {
-    return (
-      <div>
-        {this.isNotMenteeOrMentor()}
-      </div>
-    )
+
+    if (this.state.profileExists) {
+      return <PersonalProfile />
+    } else {
+      return <CreateProfile action={this.profileCreated}/>
+    }
+    
   }
 }
 
